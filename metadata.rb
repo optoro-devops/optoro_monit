0106fbb343fc1c0170c52b4ddf075ed3cfa96855
Alex Freidah <afreidah@optoro.com>
2014-10-09 10:51:00 -0400

    updated metadata to not depend on community version of monit cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,4 @@ maintainer_email 'devops@optoro.com'
 license          'All rights reserved'
 description      'Installs/Configures optoro_monit'
 long_description 'Installs/Configures optoro_monit'
-version          '0.1.0'
-
-depends 'monit', '~> 0.7.1'
+version          '0.1.1'
